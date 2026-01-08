bd6046f4129a49f8bb429126a3bdfde9ce3b5486
Guilherme Saud <62403672+Gsaudx@users.noreply.github.com>
2026-01-08 19:51:45 -0300

    style(frontend): Formatted files with prettier

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { HealthCheckPage } from '@/features/health-check';
 //! EXAMPLE
-// import { 
-//   DashboardPage, 
-//   AnalyticsPage, 
-//   ReportsPage 
+// import {
+//   DashboardPage,
+//   AnalyticsPage,
+//   ReportsPage
 // } from '@/features/dashboard';
 
 export function AppRoutes() {
@@ -14,14 +14,13 @@ export function AppRoutes() {
         <Route path="/" element={<Navigate to="/healthcheck" replace />} />
         {/* Simple route example */}
         <Route path="/healthcheck" element={<HealthCheckPage />} />
-        
+
         {/* Nested routes example */}
         {/* <Route path="/dashboard">
           <Route index element={<DashboardPage />} />
           <Route path="analytics" element={<AnalyticsPage />} />
           <Route path="reports" element={<ReportsPage />} />
         </Route> */}
-
       </Routes>
     </BrowserRouter>
   );
