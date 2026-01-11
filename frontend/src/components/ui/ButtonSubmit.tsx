7519528c4a7f3297bbe0005afe9fe510a3804965
Guilherme Saud <62403672+Gsaudx@users.noreply.github.com>
2026-01-10 23:07:06 -0300

    style(frontend): Run prettier to standardize code

--- frontend/src/components/ui/ButtonSubmit.tsx
+++ frontend/src/components/ui/ButtonSubmit.tsx
@@ -1,18 +1,24 @@
 type ButtonSubmitProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   full?: boolean;
-  children?: React.ReactNode ;
+  children?: React.ReactNode;
 };
 
-export default function ButtonSubmit({ full = false, className = "", children, ...props }: ButtonSubmitProps) {
+export default function ButtonSubmit({
+  full = false,
+  className = '',
+  children,
+  ...props
+}: ButtonSubmitProps) {
   return (
     <button
-      className={`${full 
-                    ? "mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg transition-colors" 
-                    : "mt-4 w-1/2 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg transition-colors"
-                } ${className}`}
+      className={`${
+        full
+          ? 'mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg transition-colors'
+          : 'mt-4 w-1/2 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg transition-colors'
+      } ${className}`}
       {...props}
     >
-      {children || "Enviar"}
+      {children || 'Enviar'}
     </button>
   );
 }
--- frontend/src/components/ui/InputNome.tsx
+++ frontend/src/components/ui/InputNome.tsx
@@ -1,11 +1,18 @@
-import Input from "./Input";
+import Input from './Input';
 
 type InputNomeProps = React.InputHTMLAttributes<HTMLInputElement> & {
-    inputId?: string;
-}
+  inputId?: string;
+};
 
 export default function InputNome({ inputId, ...props }: InputNomeProps) {
-    return (
-        <Input nomeLabel="Nome" tipoInput="text" placeholderInput="Digite seu nome" tamMax={40} inputId={inputId} {...props} />
-    )
+  return (
+    <Input
+      nomeLabel="Nome"
+      tipoInput="text"
+      placeholderInput="Digite seu nome"
+      tamMax={40}
+      inputId={inputId}
+      {...props}
+    />
+  );
 }
