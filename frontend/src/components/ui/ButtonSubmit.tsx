614c0d4b30c2f5433778f7da85d224b0062b1d8f
Isaque Precioso <caracktergames@gmail.com>
2026-01-12 20:12:48 -0300

    style: changes on text font and color

--- frontend/src/components/ui/ButtonSubmit.tsx
+++ frontend/src/components/ui/ButtonSubmit.tsx
@@ -1,23 +1,26 @@
 type ButtonSubmitProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   full?: boolean;
   children?: React.ReactNode;
+  icon?: React.ReactNode;
 };
 
 export default function ButtonSubmit({
   full = false,
   className = '',
   children,
+  icon,
   ...props
 }: ButtonSubmitProps) {
   return (
     <button
       className={`${
-        full
-          ? 'mt-4 w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2.5 sm:py-2 px-4 rounded-lg transition-colors'
-          : 'mt-4 w-full sm:w-1/2 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2.5 sm:py-2 px-4 rounded-lg transition-colors'
+      full
+        ? 'mt-4 w-full bg-blue-600 hover:bg-blue-700 text-white py-2.5 sm:py-2 px-4 rounded-lg transition-colors flex items-center justify-center'
+        : 'mt-4 w-full sm:w-1/2 bg-blue-600 hover:bg-blue-700 text-white py-2.5 sm:py-2 px-4 rounded-lg transition-colors flex items-center justify-center'
       } ${className}`}
       {...props}
     >
+      {icon && <span className="mr-2">{icon}</span>}
       {children || 'Enviar'}
     </button>
   );
