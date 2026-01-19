72002ce5d71cae8f3bf921004ed3c4f7f2363fc4
Isaque Precioso <caracktergames@gmail.com>
2026-01-19 19:05:57 -0300

    Update ClientModal.tsx

--- frontend/src/features/clients-page/components/ClientModal.tsx
+++ frontend/src/features/clients-page/components/ClientModal.tsx
@@ -1,9 +1,6 @@
 import ModalBase from '@/components/layout/ModalBase';
 import { inviteStatusLabels, type Client } from '../types';
 import {
-  Mail,
-  Phone,
-  CreditCard,
   Calendar,
   Pencil,
   Trash2,
@@ -19,12 +16,6 @@ interface ClientModalProps {
   size?: 'sm' | 'md' | 'lg' | 'xl' | 'xxl';
   onSwitchToEdit?: () => void;
   onSwitchToDelete?: () => void;
-}
-
-function formatCpf(cpf: string): string {
-  const digits = cpf.replace(/\D/g, '');
-  if (digits.length !== 11) return cpf;
-  return digits.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
 }
 
 function formatDate(dateString: string): string {
